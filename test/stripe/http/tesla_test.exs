4e9d1f1eba839a4a45f613dc6b5173d90749b69a
Matthew Kuntz <matt@mfkuntz.com>
2026-03-12 14:00:22 -0700

    tesla test

---
+++ test/stripe/http/tesla_test.exs
@@ -0,0 +1,42 @@
+defmodule Stripe.HTTP.TeslaTest do
+  use ExUnit.Case, async: true
+
+  describe "supervisor_children/0" do
+    test "returns Finch child spec" do
+      if Code.ensure_loaded?(Tesla) and Code.ensure_loaded?(Finch) do
+        children = Stripe.HTTP.Tesla.supervisor_children()
+        assert length(children) == 1
+      end
+    end
+  end
+
+  describe "request/5 integration" do
+    @tag :tesla_only
+    test "makes a successful request through Tesla" do
+      if Code.ensure_loaded?(Tesla) and Code.ensure_loaded?(Finch) do
+        api_base_url = Application.get_env(:stripity_stripe, :api_base_url)
+
+        case Stripe.HTTP.Tesla.request(
+               :get,
+               "#{api_base_url}/v1/customers",
+               [
+                 {"Authorization", "Bearer sk_test_123"},
+                 {"Content-Type", "application/x-www-form-urlencoded"}
+               ],
+               "",
+               []
+             ) do
+          {:ok, status, headers, body} ->
+            assert status in 200..299
+            assert is_binary(body)
+            # Verify headers are returned as a list of tuples
+            assert Enum.all?(headers, fn {k, v} -> is_binary(k) and is_binary(v) end)
+
+          {:error, :econnrefused} ->
+            # stripe-mock may not be running, that's ok
+            :ok
+        end
+      end
+    end
+  end
+end
